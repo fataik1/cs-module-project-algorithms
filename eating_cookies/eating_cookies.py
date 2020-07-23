cb7575cb5043c41898a23ad035d916eb853f1a47
Fatai King <kinggy89@gmail.com>
2020-07-23 01:22:07 -0400

    hw

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,6 +4,13 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    if  n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+
 
     pass
 
